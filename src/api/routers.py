94c86a892a984e8c45e8cfa7f721b8e83cf3f17b
garibaldiviolin <garibaldiviolin@gmail.com>
2023-08-30 14:46:18 -0300

    Move endpoints to separate routers module

---
+++ src/api/routers.py
@@ -0,0 +1,37 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Query
+
+from api import databases, models
+
+router = APIRouter()
+
+
+@router.post("/items/")
+async def add_item(item: models.Item):
+    return await databases.add_item(item)
+
+
+@router.get("/items/")
+async def get_items(
+    limit: Annotated[int, Query(gt=0, lt=20)] = 2,
+    offset: int = 0,
+):
+    return await databases.get_items(limit, offset)
+
+
+@router.get("/items/{item_id}/")
+async def get_item(item_id: int):
+    return await databases.get_item(item_id)
+
+
+@router.put("/items/{item_id}/")
+async def modify_item(item_id: int, modified_item: models.ModifiedItem):
+    return await databases.modify_item(item_id, modified_item)
+
+
+@router.patch("/items/{item_id}/")
+async def modify_item_partially(
+    item_id: int, partially_modified_item: models.PartiallyModifiedItem
+):
+    return await databases.modify_item_partially(item_id, partially_modified_item)
--- src/main.py
+++ src/main.py
@@ -1,37 +1,6 @@
-from typing import Annotated
+from fastapi import FastAPI
 
-from fastapi import FastAPI, Query
-
-from api import databases, models
+from api.routers import router
 
 app = FastAPI()
-
-
-@app.post("/items/")
-async def add_item(item: models.Item):
-    return await databases.add_item(item)
-
-
-@app.get("/items/")
-async def get_items(
-    limit: Annotated[int, Query(gt=0, lt=20)] = 2,
-    offset: int = 0,
-):
-    return await databases.get_items(limit, offset)
-
-
-@app.get("/items/{item_id}/")
-async def get_item(item_id: int):
-    return await databases.get_item(item_id)
-
-
-@app.put("/items/{item_id}/")
-async def modify_item(item_id: int, modified_item: models.ModifiedItem):
-    return await databases.modify_item(item_id, modified_item)
-
-
-@app.patch("/items/{item_id}/")
-async def modify_item_partially(
-    item_id: int, partially_modified_item: models.PartiallyModifiedItem
-):
-    return await databases.modify_item_partially(item_id, partially_modified_item)
+app.include_router(router)
