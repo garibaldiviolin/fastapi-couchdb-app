a08858f5df4c94ea55593f4879f39eb863d78ed9
garibaldiviolin <garibaldiviolin@gmail.com>
2023-08-30 21:33:33 -0300

    Add tests to GET and POST methods

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -34,28 +34,45 @@ def database():
     asyncio.run(delete_database())
 
 
-async def create_item(existing_item_id):
+@pytest.fixture
+def item_dict():
+    return {
+        "name": "soap3",
+        "description": "This is a soap",
+        "price": 1.54,
+    }
+
+
+async def create_item(item_id, item_dict):
     async with aiocouch.CouchDB(
         settings.COUCHDB_URL,
         user=settings.COUCHDB_USER,
         password=settings.COUCHDB_PASSWORD,
     ) as couchdb:
         db = await couchdb[COUCHDB_TEST_DATABASE]
-        item = {
-            "name": "soap3",
-            "description": "This is a soap",
-            "price": 1.54,
-        }
-        new_doc = await db.create(databases.format_item_id(existing_item_id), data=item)
+        item = item_dict.copy()
+        new_doc = await db.create(databases.format_item_id(item_id), data=item)
         await new_doc.save()
         return item
 
 
 @pytest.fixture
-def existing_item_id():
+def item_id():
     return 81238
 
 
 @pytest.fixture
-def existing_item(database, existing_item_id):
-    return asyncio.run(create_item(existing_item_id))
+def item_id2():
+    return 81239
+
+
+@pytest.fixture
+def item(database, item_id, item_dict):
+    return asyncio.run(create_item(item_id, item_dict))
+
+
+@pytest.fixture
+def items(database, item_id, item_id2, item_dict):
+    item = asyncio.run(create_item(item_id, item_dict))
+    item2 = asyncio.run(create_item(item_id2, item_dict))
+    return item, item2
--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -5,7 +5,39 @@ from main import app
 client = TestClient(app)
 
 
-def test_get_item(existing_item, existing_item_id):
-    response = client.get(f"/items/{existing_item_id}/")
+def test_get_item(item, item_id):
+    response = client.get(f"/items/{item_id}/")
     assert response.status_code == 200
-    assert response.json() == existing_item
+    assert response.json() == item
+
+
+def test_get_item_with_inexistent_id(database):
+    response = client.get("/items/999/")
+    assert response.status_code == 404
+    assert response.json() == {"detail": "item_not_found"}
+
+
+def test_get_items_without_any(database):
+    response = client.get("/items/")
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_get_items(items, item_dict):
+    response = client.get("/items/")
+    assert response.status_code == 200
+    assert response.json() == [item for item in items]
+
+
+def test_add_item(database, item_dict, item_id):
+    item_dict["id"] = item_id
+    response = client.post("/items/", json=item_dict)
+    assert response.status_code == 201
+    assert response.json() == item_dict
+
+
+def test_add_item_with_existing_id(item, item_dict, item_id):
+    item_dict["id"] = item_id
+    response = client.post("/items/", json=item_dict)
+    assert response.status_code == 400
+    assert response.json() == {"detail": "item_already_exists"}
