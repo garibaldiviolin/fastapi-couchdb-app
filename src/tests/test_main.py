823cbef4d2549fbf729bbcb6505c13306358b01c
garibaldiviolin <garibaldiviolin@gmail.com>
2023-08-30 21:03:18 -0300

    Change test method name

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -5,7 +5,7 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_item(existing_item, existing_item_id):
+def test_get_item(existing_item, existing_item_id):
     response = client.get(f"/items/{existing_item_id}/")
     assert response.status_code == 200
     assert response.json() == existing_item
