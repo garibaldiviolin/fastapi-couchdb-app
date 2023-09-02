8a30a479bd75787bfa39d046165a70ccb6a86663
garibaldiviolin <garibaldiviolin@gmail.com>
2023-09-02 12:34:33 -0300

    Add initial OAuth2 and JWT code

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 
-from api.routers import router
+from api.routers import router as api_router
+from security.routers import router as security_router
 
 app = FastAPI()
-app.include_router(router)
+app.include_router(api_router)
+app.include_router(security_router)
